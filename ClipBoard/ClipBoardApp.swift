62c222657d751aa1ec1b579f1a5ffdae112f0f14
ricopenkalski <51368644+ricopenkalski@users.noreply.github.com>
2025-04-15 02:07:08 +0200

    Initial Commit

---
+++ ClipBoard/ClipBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipBoardApp.swift
+//  ClipBoard
+//
+//  Created by Rico Penkalski on 15.04.25.
+//
+
+import SwiftUI
+
+@main
+struct ClipBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipBoardTests/ClipBoardTests.swift
@@ -0,0 +1,16 @@
+//
+//  ClipBoardTests.swift
+//  ClipBoardTests
+//
+//  Created by Rico Penkalski on 15.04.25.
+//
+
+import Testing
+
+struct ClipBoardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
