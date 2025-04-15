523a47b8c64aa8e9ad9af888a0db309caa929e97
ricopenkalski <51368644+ricopenkalski@users.noreply.github.com>
2025-04-15 02:36:38 +0200

    Add AppDelegate for clipboard monitoring and menu bar support

---
+++ ClipBoard/AppDelegate.swift
@@ -0,0 +1,85 @@
+//
+//  AppDelegate.swift
+//  ClipBoard
+//
+//  Created by Rico Penkalski on 15.04.25.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var statusItem: NSStatusItem?
+    var clipboardHistory: [String] = []
+    var lastClipboardContent: String = ""
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        setupMenuBar()
+        startClipboardMonitoring()
+    }
+
+    func setupMenuBar() {
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+
+        if let button = statusItem?.button {
+            button.image = NSImage(systemSymbolName: "doc.on.doc", accessibilityDescription: "Clipboard Manager")
+        }
+
+        updateMenu()
+    }
+
+    func updateMenu() {
+        let menu = NSMenu()
+
+        if clipboardHistory.isEmpty {
+            let emptyItem = NSMenuItem(title: "Keine Einträge", action: nil, keyEquivalent: "")
+            emptyItem.isEnabled = false
+            menu.addItem(emptyItem)
+        } else {
+            for entry in clipboardHistory.prefix(5) {
+                let item = NSMenuItem(title: entry, action: #selector(copyEntry(_:)), keyEquivalent: "")
+                item.target = self
+                menu.addItem(item)
+            }
+        }
+
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Settings", action: #selector(openSettings), keyEquivalent: ","))
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(quitApp), keyEquivalent: "q"))
+
+        statusItem?.menu = menu
+    }
+
+    func startClipboardMonitoring() {
+        lastClipboardContent = NSPasteboard.general.string(forType: .string) ?? ""
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(checkClipboard), userInfo: nil, repeats: true)
+    }
+
+    @objc func checkClipboard() {
+        guard let content = NSPasteboard.general.string(forType: .string) else { return }
+
+        if content != lastClipboardContent {
+            //insert the new clipboard content
+            lastClipboardContent = content
+            clipboardHistory.insert(content, at: 0)
+            // remove the last item if its to big
+            if clipboardHistory.count > 50 {
+                clipboardHistory.removeLast()
+            }
+            updateMenu()
+        }
+    }
+    // copy the clicked entry
+    @objc func copyEntry(_ sender: NSMenuItem) {
+        NSPasteboard.general.clearContents()
+        NSPasteboard.general.setString(sender.title, forType: .string)
+    }
+    
+    
+    @objc func openSettings() {
+        print("Settings clicked!")
+    }
+
+    @objc func quitApp() {
+        NSApplication.shared.terminate(nil)
+    }
+}
--- ClipBoard/ClipBoardApp.swift
+++ ClipBoard/ClipBoardApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct ClipBoardApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
